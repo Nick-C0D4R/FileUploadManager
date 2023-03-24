509f8308e1c7286929f3f5920a9f52ae036b21c9
Mykola Kovshar <knikolay20031230@gmail.com>
2023-03-24 16:27:00 +0200

    Add service for uploading file

---
+++ FileUploadManager/Data/FileUploadService.cs
@@ -0,0 +1,25 @@
+﻿using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace FileUploadManager.Data
+{
+    public class FileUploadService
+    {
+        private BlobContainerClient _client;
+
+        public FileUploadService(BlobContainerClient client)
+        {
+            _client = client;
+        }
+
+        public async Task<bool> Load(string fileName, IBrowserFile file)
+        {
+            var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            string connectionString = config.GetSection("AzureSettings")["ConnectionString"];
+            string storageName = config.GetSection("AzureSettings")["BlobName"];
+            BlobClient blobClient = _client.GetBlobClient(fileName);
+            await blobClient.UploadAsync(file.OpenReadStream());
+            return true;
+        }
+    }
+}
