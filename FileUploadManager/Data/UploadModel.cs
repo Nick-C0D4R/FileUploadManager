768fcf5af97b8234853989c8c647cf09f24d8e44
Mykola Kovshar <knikolay20031230@gmail.com>
2023-03-24 16:25:36 +0200

    Add UploadModel (Contains File & Email properties)

---
+++ FileUploadManager/Data/UploadModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using System.ComponentModel.DataAnnotations;
+
+namespace FileUploadManager.Data
+{
+    public class UploadModel
+    {
+        [Required]
+        public IBrowserFile File { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
